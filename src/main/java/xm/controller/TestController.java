f357800be00001eae8bb8daf6d249db5fbbcb0c3
maquanyang <qyma@bbw-portnet.com>
2019-10-30 13:25:47 +0800

    测试自动构建                           --马权阳

--- src/main/java/xm/controller/TestController.java
+++ src/main/java/xm/controller/TestController.java
@@ -28,6 +28,7 @@ public class TestController {
         System.out.println("test3-----test3");
         System.out.println("test4-----test4");
         System.out.println("test5-----test5");
+        t.setName("xiaoma");
         return JSONObject.toJSONString(t);
     }
 }
